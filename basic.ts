09568f05aee2abbb2c04639f38de1d2115f19439
Moira Leon <moira.leon2@gmail.com>
2021-10-02 17:06:44 -0700

    creating js folder and tsconfig.json

--- basic.ts
+++ basic.ts
@@ -16,3 +16,8 @@ let webHeading = document.querySelector('h1');
 webHeading!.textContent=newMessage;
 
 // sudo npm install -g typescript
+//other  tsc commands
+// tsc basic.ts - creates typescript replica
+// tsc --target ES 2015 --ourDir js basic.ts (creates generated version of EcmaScript 2915 and puts new js in a dir(folder) called js)
+
+//compiler options are cusotmizable-- instead of writing this each time we can create a typescript configuration file called tsconfig.json it is just a plain text file in json format with keys and values
